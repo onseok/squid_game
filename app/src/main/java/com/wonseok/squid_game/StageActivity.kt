627115ca279eb3624f86ec03a960cebd7451c991
Won Seok Kim <76798309+dnjstjr245@users.noreply.github.com>
2021-10-06 22:10:29 +0900

    feat: rank system, character motion

--- app/src/main/java/com/wonseok/squid_game/StageActivity.kt
+++ app/src/main/java/com/wonseok/squid_game/StageActivity.kt
@@ -1,18 +1,40 @@
 package com.wonseok.squid_game
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
 import com.wonseok.squid_game.databinding.ActivityStageBinding
 import com.wonseok.squid_game.databinding.ActivityStageBinding.inflate
+import java.util.*
+import kotlin.concurrent.timer
 
 class StageActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityStageBinding
+    private var time = 0
+    private var timerTask: Timer? = null
+    private val mDelayHandler: Handler by lazy { Handler(Looper.getMainLooper()) }
+    private var playerScore = 0
+    private var playerNickName = ""
+    private var runLeftFlag = false
+    private var runRightFlag = false
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = inflate(layoutInflater)
         setContentView(binding.root)
+
+        val window = window
+        window.setFlags( // 풀스크린
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
 
         binding.characterProfileNameTextView.text = intent.getStringExtra("profileName")
         when (intent.getStringExtra("profileName")) {
@@ -25,5 +47,145 @@ class StageActivity : AppCompatActivity() {
             "새벽" -> binding.characterProfileImageView.setImageResource(R.drawable.character_saebyeok_profile)
             "지영" -> binding.characterProfileImageView.setImageResource(R.drawable.character_jiyeong_profile)
         }
+
+        setStartDialog()
+
+
+        binding.leftRunButton.setOnTouchListener { _: View, event: MotionEvent ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> { // 왼쪽 버튼 터치했을 때
+                    binding.characterMotionImageView.setImageResource(R.drawable.left_motion)
+                }
+                MotionEvent.ACTION_UP -> { // 왼쪽 버튼 누르고 땟을 때
+                    binding.characterMotionImageView.setImageResource(R.drawable.left_after_motion)
+                    if (!runLeftFlag) {
+                        playerScore++
+                        binding.playerScoreNumberTextView.text = playerScore.toString()
+                    }
+                    runLeftFlag = true
+                    runRightFlag = false
+                }
+            }
+            true
+        }
+
+        binding.rightRunButton.setOnTouchListener { _: View, event: MotionEvent ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> { // 오른쪽 버튼 터치했을 때
+                    binding.characterMotionImageView.setImageResource(R.drawable.right_motion)
+                }
+                MotionEvent.ACTION_UP -> { // 오른쪽 버튼 누르고 땟을 때
+                    binding.characterMotionImageView.setImageResource(R.drawable.right_after_motion)
+                    if (!runRightFlag) {
+                        playerScore++
+                        binding.playerScoreNumberTextView.text = playerScore.toString()
+                    }
+                    runRightFlag = true
+                    runLeftFlag = false
+                }
+            }
+            true
+        }
+
+    }
+
+    // 시작 다이얼로그 버튼 불러오기
+    private fun setStartDialog() {
+        val dialog = StartDialogFragment()
+        dialog.isCancelable = false
+        dialog.show(supportFragmentManager, "DialogStartFragment")
+    }
+
+    // 시간 끝나거나 죽으면 다이얼로그 세팅
+    private fun setDialog() {
+        // 죽는다면 리셋 다이얼로그
+        if(binding.playerLifeTextView.text == "")
+            mDelayHandler.post(::setResetDialog)
+
+        // 죽지않고 시간이 끝난다면
+        else if(binding.playerLifeTextView.text == "♥")
+            mDelayHandler.post(::setRankDialog)
+    }
+
+    // 다 멈추고, 리셋 다이얼로그 버튼 불러오기
+    private fun setResetDialog() {
+        stopTimer() // 타이머 멈추고
+        mDelayHandler.removeCallbacksAndMessages(null) // 과녁 이미지 동작 멈춤
+        val score = playerScore
+
+//        val dialog = ResetDialogFragment(score)
+//        dialog.isCancelable = false
+//        dialog.show(supportFragmentManager, "DialogResetFragment")
+    }
+
+    // three stage 까지 완 -> Rank 판 보여주기
+    private fun setRankDialog() {
+        stopTimer() // 타이머 멈추고
+        mDelayHandler.removeCallbacksAndMessages(null) // 과녁 이미지 동작 멈춤
+        val score = playerScore
+
+        val dialog = RankDialogFragment(playerNickName, score)
+        dialog.isCancelable = false
+        dialog.show(supportFragmentManager, "DialogRankFragment")
+    }
+
+    // 게임 뷰 세팅하기
+    fun setGameView(playerNickName: String) {
+//        binding.mainConstraintLayout.setPadding(0, statusBarHeight(this), navigationBarHeight(this), 0)
+//        Glide.with(this).load(R.raw.cat_player_01).override(250,250).into(binding.playerImageView)
+//
+
+        binding.characterProfileImageView.visibility = View.VISIBLE
+        binding.characterProfileNameTextView.visibility = View.VISIBLE
+        binding.playerLifeTextView.visibility = View.VISIBLE
+        binding.timeLifeMinuteTextView.visibility = View.VISIBLE
+        binding.timeLifeSecondTextView.visibility = View.VISIBLE
+        binding.playerScoreNumberTextView.visibility = View.VISIBLE
+        binding.playerNicknameTextView.visibility = View.VISIBLE
+        binding.characterMotionImageView.visibility = View.VISIBLE
+        binding.leftRunButton.visibility = View.VISIBLE
+        binding.rightRunButton.visibility = View.VISIBLE
+
+        binding.playerScoreNumberTextView.text = playerScore.toString()
+
+//        binding.targetImageView.setImageResource(R.drawable.ic_target)
+
+        binding.playerLifeTextView.text = "♥" // 생명 세팅 ( 목숨은 1개 )
+        binding.playerNicknameTextView.text = playerNickName
+//        lifeLength = binding.playerLifeTextView.length()
+        this.playerNickName = playerNickName // 닉네임 세팅
+
+//
+//        targetPositionX = binding.targetImageView.x
+//        arrowPositionX = binding.arrowImageView.x
+        startTimer()
+//
+//        if(imageStatus)
+//            downImageMove()
+//        else
+//            upImageMove()
+    }
+
+    // 타이머 동작
+    private fun startTimer() {
+        timerTask = timer(period = 10) {
+            time++
+            val sec = 59 - (time / 100)
+
+            runOnUiThread {
+                if (sec < 10)
+                    binding.timeLifeSecondTextView.text = "0$sec"
+                else
+                    binding.timeLifeSecondTextView.text = "$sec"
+            }
+
+            if(sec == 0)
+                mDelayHandler.post(::setDialog)
+        }
+    }
+
+    private fun stopTimer() {
+        timerTask?.cancel() // 타이머 정지
+        time = 0
     }
 }
---
+++ app/src/main/java/com/wonseok/squid_game/StartDialogFragment.kt
@@ -0,0 +1,103 @@
+package com.wonseok.squid_game
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.text.InputFilter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
+import com.wonseok.squid_game.databinding.FragmentStartDialogBinding
+import com.wonseok.squid_game.sqlite.DBHelper
+
+class StartDialogFragment : DialogFragment() {
+    private var _binding: FragmentStartDialogBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var dbHelper : DBHelper
+
+    private val mDelayHandler: Handler by lazy { Handler() } // 과녁 이미지 딜레이 관련
+    private var playerNickName = ""
+
+    private var imm : InputMethodManager? = null // 키보드 InputMethodManager
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStartDialogBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        imm = activity?.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+
+        // layout background transparency
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        // click name tag
+        binding.nickNameTextView.setOnClickListener {
+            binding.nickNameEditText.requestFocus()
+            imm?.showSoftInput(binding.nickNameEditText, 0)
+        }
+
+        // name english only
+        onlyAlphabetFilterToEnglishET()
+
+        // click game start button
+        binding.startImageView.setOnClickListener {
+            playerNickName = binding.nickNameEditText.text.toString()
+
+            if(playerNickName.isNotEmpty()) {
+                if(checkSameNickName())
+                    Toast.makeText(context, "💡 중복된 닉네임입니다", Toast.LENGTH_SHORT).show()
+                else
+                    mDelayHandler.postDelayed(::startGame, 1000L)
+            }
+            else
+                Toast.makeText(context, "🧙‍♀️ 이름을 입력해주세요", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
+    }
+
+    private fun startGame() {
+        dismiss()
+        (context as StageActivity).setGameView(playerNickName)
+    }
+
+    private fun checkSameNickName(): Boolean {
+        dbHelper = DBHelper(context, "RankDB", null, 1)
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery("select nickName from RankDB where nickName = '$playerNickName'", null)
+
+        if(cursor.count > 0)
+            return true
+
+        return false
+    }
+
+    // write nickName english only
+    private fun onlyAlphabetFilterToEnglishET() {
+        binding.nickNameEditText.setFilters(arrayOf(
+            InputFilter { src, start, end, dst, dstart, dend ->
+                if (src == " ") { // for space
+                    return@InputFilter src
+                }
+                if (src == "") { // for backspace
+                    return@InputFilter src
+                }
+                if (src.matches(Regex("[a-zA-Z]+"))) {
+                    return@InputFilter src
+                }
+                Toast.makeText(requireContext(), "🧙‍♀️ 영어로 입력해주세요", Toast.LENGTH_SHORT).show()
+                binding.nickNameEditText.setText("")
+                return@InputFilter ""
+            }
+        ))
+    }
+}
